5d2b6439cfd48abc04f0a6064ae3b06a107c4432
Michelle Fernandez <fernandezm525@outlook.es>
2023-04-24 21:57:36 -0500

    prueba3

--- KingCourier/settings.py
+++ KingCourier/settings.py
@@ -25,7 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = 'django-insecure-&mpz7oq@db&h&30+-6@mvx^qf9kab$rs8xs-x29c8nx@s=99a5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
