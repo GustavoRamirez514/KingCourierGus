a75c647d1373c3b227f0f871aba0ca67742feb9a
GustavoRamirez514 <tavo200079.gr@gmail.com>
2023-05-30 11:51:40 -0500

    cambios en el secret key y el debug

--- KingCourier/settings.py
+++ KingCourier/settings.py
@@ -27,10 +27,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+# DESPLIEGUE
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+# DEBUG = os.environ.get('DEBUG')
+# DESPLIEGUE
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 # DESPLIEGUE
